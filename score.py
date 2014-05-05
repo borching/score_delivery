927d3fae5dad377dfa9e8cf566e0c6ab7f1f17c0
Borching Su <borching@gmail.com>
2014-05-05 23:39:15 +0800

    Almost ready

--- score.py
+++ score.py
@@ -25,7 +25,7 @@ smtpserver.login(gmail_user, gmail_pwd)
 #寄件人資訊
 fromaddr = "borching@gmail.com"
 
-for i in range(2):
+for i in range(23):
 #組員信箱    
     x=int(wks.cell(3+i,3).value)
     if x==3:
@@ -35,14 +35,14 @@ for i in range(2):
     toaddrs.append("borching@gmail.com")
     
 #內容
-    str = """<head><style>table, th, td{border-collapse:collapse;border:1px solid black;}th,td{padding:15px;}</style></head>"""
+    str = "<html><head><style>table, th, td{border-collapse:collapse;border:1px solid black;}th,td{padding:15px;}</style></head><body>To:"
     str = str + ", ".join(toaddrs)
     str = str + """<table style="width:1200px">"""
     s = '[收到請回信確認] 線性代數 4/23 課堂作業批改結果 (組員:'+wks.cell(3+i,4).value+','+wks.cell(3+i,7).value+','+wks.cell(3+i,10).value+',本次得分'+wks.cell(3+i,2).value+')'
     for j in range(2):
         print(j);
         str=str+"<tr><td>"+(wks.cell(1,j+1).value or " ")+"</td><td>"+(wks.cell(3+i,j+1).value or " ")+"</td><td>"+(wks.cell(2,j+1).value or " ")+"</td></tr>"
-    str=str+"</table>"
+    str = str + "</table></body></html>"
     msg=MIMEText(str,'html','utf-8') 
     msg['Subject'] = s
 
