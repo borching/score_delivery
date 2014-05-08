34ac0ef75f5209e05409c602886a360fe362663e
Borching Su <borching@gmail.com>
2014-05-08 16:20:55 +0800

    printing relevant information during processing..

--- param.py
+++ param.py
@@ -1,4 +1,8 @@
 worksheet = "線性代數 4/30 課堂作業批改結果"
-mailTitle = "[收到請不用回信確認] 線性代數 4/30 課堂作業批改結果 "
+mailTitle = "[收到不用再回信確認] 線性代數 4/30 課堂作業批改結果 "
+mailFirstSentence = "<BR> 請各位同學多利用此批改結果作為複習課堂上練習過題目的參考。尤其是寫錯的部分，一定要重新想一遍弄清楚喔！<BR>"
+mailFirstSentence = mailFirstSentence + "<BR> 歡迎提供此自動信件之改善意見。我將視大家的需求與自己時間許可盡量更新。<BR>"
+mailFirstSentence = mailFirstSentence + "假如你對寫程式有興趣，願意幫忙把此自動信件變得更好的話，請直接與老師聯絡。<BR>"
 userRange = range(1)
 cellRange = range(70)
+test = 1
--- score.py
+++ score.py
@@ -16,10 +16,12 @@ address = '@ntu.edu.tw'
 gc = gspread.login(gmail_user,gmail_pwd)
 
 #開啟的文件
+print("Connecting to the worksheet" + worksheet + "....")
 wks = gc.open(worksheet).sheet1
-
+print("Worksheet successfully connected")
 
 #GMAIL的SMTP伺服器
+print("Connecting to the SMTP server....")
 smtpserver = smtplib.SMTP("smtp.gmail.com",587)
 smtpserver.ehlo()
 smtpserver.starttls()
@@ -27,6 +29,7 @@ smtpserver.ehlo()
 
 #登入系統
 smtpserver.login(gmail_user, gmail_pwd)
+print("SMTP server successfully connected")
 
 #寄件人資訊
 fromaddr = "borching@gmail.com"
@@ -43,18 +46,20 @@ for i in userRange:
 #內容
     str = "<html><head><style>table, th, td{border-collapse:collapse;border:1px solid black;}th,td{padding:15px;}</style></head><body>To:"
     str = str + ", ".join(toaddrs)
+    str = str + mailFirstSentence
     str = str + """<table style="width:1200px">"""
     s = mailTitle
     s = s + '(組員:'+wks.cell(3+i,4).value+','+wks.cell(3+i,7).value+','+wks.cell(3+i,10).value+',本次得分'+wks.cell(3+i,2).value+')'
     for j in cellRange:
-        print(j);
+        print(j, end=" ");
         str=str+"<tr><td>"+(wks.cell(1,j+1).value or " ")+"</td><td>"+(wks.cell(3+i,j+1).value or " ")+"</td><td>"+(wks.cell(2,j+1).value or " ")+"</td></tr>"
     str = str + "</table></body></html>"
     msg=MIMEText(str,'html','utf-8') 
     msg['Subject'] = s
 
 #測試信箱
-#    toaddrs = ['borching@gmail.com']
+    if test == 1:
+        toaddrs = ['borching@gmail.com']
        
 #設定寄件資訊
     smtpserver.sendmail(fromaddr, toaddrs, msg.as_string())
