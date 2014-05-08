9b888bd5c4609df611dfaabdd0ab10f3c6434e34
Borching Su <borching@gmail.com>
2014-05-09 01:59:05 +0800

    Sending 5/7's results

--- param.py
+++ param.py
@@ -1,8 +1,18 @@
-worksheet = "線性代數 4/30 課堂作業批改結果"
-mailTitle = "[收到不用再回信確認] 線性代數 4/30 課堂作業批改結果 "
+#worksheet = "線性代數 4/30 課堂作業批改結果"
+#mailTitle = "[收到不用再回信確認] 線性代數 4/30 課堂作業批改結果 "
+#mailFirstSentence = "<BR> 請各位同學多利用此批改結果作為複習課堂上練習過題目的參考。尤其是寫錯的部分，一定要重新想一遍弄清楚喔！<BR>"
+#mailFirstSentence = mailFirstSentence + "<BR> 歡迎提供此自動信件之改善意見。我將視大家的需求與自己時間許可盡量更新。<BR>"
+#mailFirstSentence = mailFirstSentence + "假如你對寫程式有興趣，願意幫忙把此自動信件變得更好的話，請直接與老師聯絡。<BR>"
+#userRange = range(24)
+#cellRange = range(74)
+#test = 0
+
+# 5 / 7
+worksheet = "線性代數 5/7 課堂作業批改結果"
+mailTitle = "[線性代數] 5/7 課堂作業批改結果 "
 mailFirstSentence = "<BR> 請各位同學多利用此批改結果作為複習課堂上練習過題目的參考。尤其是寫錯的部分，一定要重新想一遍弄清楚喔！<BR>"
 mailFirstSentence = mailFirstSentence + "<BR> 歡迎提供此自動信件之改善意見。我將視大家的需求與自己時間許可盡量更新。<BR>"
 mailFirstSentence = mailFirstSentence + "假如你對寫程式有興趣，願意幫忙把此自動信件變得更好的話，請直接與老師聯絡。<BR>"
 userRange = range(24)
-cellRange = range(74)
+cellRange = range(83)
 test = 0
